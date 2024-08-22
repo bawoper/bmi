235425e6b62f44330f8e486befd3e1f6c13fa8ac
Andrey <bawoper@gmail.com>
2024-08-22 16:25:15 +0300

    fix  error

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(double height, double weight) {
-        double amount = height * 2;
+        double amount = height * height;
         double index = weight / amount;
         int bmi = (int) index;
         return bmi;
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double height = 1.98;
-        double weight = 76;
+        double height = 1.87;
+        double weight = 98;
         int bmi = service.calculate(height, weight);
         System.out.println(bmi + "_BMI");
     }
